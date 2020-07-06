e37414188e18f25dadcf090d0281d73ef5577878
venceslau48 <darthvences@gmail.com>
2020-07-06 21:53:19 +0100

    Final commit

---
+++ build/precache-manifest.ab7fcc0e3f7fb4ab1f3a7db5ab3a56b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dbeef9de026c24d5a713a07990c09e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a618b183ddbc9d7d6a3",
+    "url": "/static/css/main.42bcd07e.chunk.css"
+  },
+  {
+    "revision": "fbb124b47781f94c588d",
+    "url": "/static/js/2.1eaf04ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1eaf04ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a618b183ddbc9d7d6a3",
+    "url": "/static/js/main.a24ae04e.chunk.js"
+  },
+  {
+    "revision": "3f27fcd83484f02df5f9",
+    "url": "/static/js/runtime-main.24fdbc9c.js"
+  }
+]);
